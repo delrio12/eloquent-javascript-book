d6acc21f6d857851f5a1701af3c22778787a1cf4
Andres Del Rio <addelrio11@gmail.com>
2024-07-31 16:55:16 -0400

    Chapter 2 completed

--- Chapter2/exercises.js
+++ Chapter2/exercises.js
@@ -1,41 +1,42 @@
 //----------------------Looping a triangle---------------------//
 console.log("write a loop that makes 7 calls to console.log to output a triangle")
 let lines = 7, msg = "#";
-for(pounds=0; pounds<lines; pounds+=1) {
+for(nums=0; nums<lines; nums+=1) {
     console.log(msg);
-    msg = msg + "#";
+    msg += "#";
 }
 //----------------------FizzBuzz---------------------//
 console.log("write program that uses console.log to print all the numbers from 1 - 100. For numbers divisible by 3, print \"Fizz\", for numbers divisible by 5, print \"Buzz\", and numbers divisible by both, print \"FizzBuzz\"");
-for(let theNumber = 1; theNumber <= 100; theNumber++) {
-    if(theNumber % 3 == 0) {
-        if(theNumber % 5 == 0) {
-            console.log("FizzBuzz");  
-        }
-        else {
-            console.log("Fizz");
-        }
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz"); 
     }
-    else if(theNumber % 5 == 0) {
+    else if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
         console.log("Buzz");
     }
     else{
-        console.log(theNumber);
+        console.log(i);
     }
 }
 //----------------------Chessboard--------------------//
 console.log("write a program that creates a string that represents 8x8 grid, using line characters to separate lines.");
-let size = 8, rowComplete = "";
+let fullRow = "";
+let size = prompt("Please input the size of the grid:");
+
 for(let rowLine = 1; rowLine <= size; rowLine++) {
     for(let columnSpace = 1; columnSpace <= size; columnSpace++) {
         if((columnSpace + rowLine) % 2 == 0) {
-            rowComplete = rowComplete + " ";  
+            fullRow += " ";  
         }
         else {
-            rowComplete = rowComplete + "#";  
+            fullRow += "#";  
         }
     }
-    console.log(rowComplete);
-    rowComplete = "";
+    console.log(fullRow);
+    fullRow = "";
 }
 
