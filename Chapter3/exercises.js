375a9f2815aa5af97be7c9da9d30737430307014
Andres Del Rio <addelrio11@gmail.com>
2024-08-01 00:28:03 -0400

    Chapter 3
    Completed first exercise

--- Chapter3/exercises.js
+++ Chapter3/exercises.js
@@ -1,5 +1,14 @@
 //----------------------Minimum---------------------//
+console.log("Write a function \'min\' that takes two arguments and returns their minimum");
+function min(num1, num2) {
+    let minimum = Math.min(num1, num2);
+    console.log(minimum);
+};
+
+min(4,3);
 
 //----------------------Recursion---------------------//
+
+
 
 //----------------------Bean Counting--------------------//
