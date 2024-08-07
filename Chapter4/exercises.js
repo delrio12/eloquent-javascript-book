3af8c00d8dca58cef82acd46f4d17f6862184e6c
Andres Del Rio <addelrio11@gmail.com>
2024-08-06 22:10:34 -0400

    Update exercises.js

--- Chapter4/exercises.js
+++ Chapter4/exercises.js
@@ -1,6 +1,6 @@
 //------------------------The Sum of a Range---------------------------//
 function range(start,end) {
-
+ for(i=0;i<end; i++)
 }
 
 function sum() {
@@ -10,6 +10,7 @@ function sum() {
 function range2() {
 
 }
+
 //---------------------------Your Own Loop-----------------------------//
 
 //----------------------------Everything------------------------------//
