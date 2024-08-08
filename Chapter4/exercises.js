a331e51b4cd858193c0055d2b8ef1a1fdb3e7ec3
Andres Del Rio <addelrio11@gmail.com>
2024-08-07 22:29:43 -0400

    Completed 4.1

--- Chapter4/exercises.js
+++ Chapter4/exercises.js
@@ -1,12 +1,28 @@
 //------------------------The Sum of a Range---------------------------//
+//---1---//
 function range(start,end) {
- for(i=0;i<end; i++)
+    let setOfNums=[];
+    for(let i=start;i<=end; i++)
+    {
+        setOfNums.push(i);
+    }
+    return setOfNums;
+     
 }
+console.log(range(1,10));
 
-function sum() {
-
+//---2---//
+function sum(arrayOfNums) {
+    result = 0;
+    for(let i=0;i<arrayOfNums.length; i++)
+    {
+        result+=arrayOfNums[i];
+    }
+    return result;
 }
+console.log(sum(range(1,10)));
 
+//---3---//
 function range2() {
 
 }
