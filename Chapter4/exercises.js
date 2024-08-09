bcfffb4acc6a88bed9401a176369f4e8cb7c6b73
Andres Del Rio <addelrio11@gmail.com>
2024-08-08 23:56:47 -0400

    Update exercises.js

--- Chapter4/exercises.js
+++ Chapter4/exercises.js
@@ -25,12 +25,23 @@ console.log(sum(range(1,10)));
 //---3---//
 function range2(start,end,step) {
     let setOfNums=[];
-    for(let i=start;i<=end; i++)
-    {
-        setOfNums.push(i);
+    
+    if(step) {
+        for(let i=start;i<=end; i+step)
+            {
+                setOfNums.push(i);
+            }
     }
+    else {
+        for(let i=start;i<=end; i++)
+            {
+                setOfNums.push(i);
+            }
+    }
+    
     return setOfNums;
 }
+console.log(sum(range2(1,10,1)));
 
 //---------------------------Your Own Loop-----------------------------//
 
