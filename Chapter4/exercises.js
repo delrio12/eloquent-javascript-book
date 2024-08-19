87031541696690ed673674ba7053fcd80275f999
Andres Del Rio <addelrio11@gmail.com>
2024-08-18 21:56:09 -0400

    Completed 4.3

--- Chapter4/exercises.js
+++ Chapter4/exercises.js
@@ -9,6 +9,7 @@ function range(start,end) {
     return setOfNums;
      
 }
+console.log('1.1');
 console.log(range(1,10));
 
 //---2---//
@@ -20,6 +21,7 @@ function sum(arrayOfNums) {
     }
     return result;
 }
+console.log('1.2');
 console.log(sum(range(1,10)));
 
 //---3---//
@@ -45,8 +47,10 @@ function range2(start,end,step) {
     
     return setOfNums;
 }
+console.log('1.3');
 console.log(range2(1,10,2));
-console.log(range2(5,2));
+console.log('1.4');
+console.log(+range2(5,2));
 
 //------------------------Reversing an Array--------------------------//
 //---1---//
@@ -57,6 +61,7 @@ function reverseArray(array1) {
     }
     return array2;
 }
+console.log('2.1');
 console.log(reverseArray([1,2,32,4,5,6,7]));
 
 //---2---//
@@ -69,24 +74,57 @@ function reverseArrayInPlace(array1) {
         }
     return array1;
 }
+console.log('2.2');
 console.log(reverseArrayInPlace([432,2,32,4,5,6,7]));
+
 //------------------------------A List--------------------------------//
+//---1---//
 var array1 = [10, 20, 30];
 function arrayToList(array) {
     var list = {};
-
- for (var i = 0; i < array.length; i++) {
-    list.value = array.splice(0, 1)[0];
-    list.rest = (array.length > 0) ? arrayToList(array) : null;
- }
-
- return list;
+    for (var i = 0; i < array.length; i++) {
+        list.value = array.splice(0, 1)[0];
+        list.rest = (array.length > 0) ? arrayToList(array) : null;
+    }
+    return list;
 }
-console.log(arrayToList(arr1));
+console.log('3.1');
+console.log(arrayToList(array1));
 
+//---2---//
+let list1 = {
+    value: 10,
+    rest: {
+        value: "dude",
+        rest: {
+            value: 30,
+            rest: 0
+        }
+    }
+};
 function listToArray(list) {
+    var result = [];
+    while(list.value){
+        result.push(list.value);
+        list = list.rest;
+    }
+    return result;  
+};
 
+console.log('3.2');
+console.log(listToArray(list1));
+
+
+function prepend(value, list) {
+    return {value, rest: list};
 }
-listToArray()
+console.log(prepend('ok', list1));
 
+
+function nth(list, n) {
+    if (!list) return undefined;
+    else if (n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+}
+console.log(nth(arrayToList([10, 20, 30]), 1));
 //---------------------------Deep Comparison--------------------------//
