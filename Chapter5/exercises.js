85ea11481afa694547e0bcb4b4e5d7b0ca44b322
Andres Del Rio <addelrio11@gmail.com>
2024-08-30 11:27:09 -0400

    Chapter 5. Ex 5.1 completed

---
+++ Chapter5/exercises.js
@@ -0,0 +1,32 @@
+//------------------------Flattening---------------------------//
+//---1---//
+// reduce(), reduce the elements of an array to a single value 
+let arrayOfArrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrayOfArrays.reduce((accumulator, next) => accumulator.concat(next)));
+
+//---2---//
+
+
+//---3---//
+
+
+//------------------------Your Own Loop--------------------------//
+//---1---//
+
+
+//---2---//
+
+
+//------------------------------Everything--------------------------------//
+//---1---//
+
+
+//---2---//
+
+
+//---3---//
+
+
+//---4---//
+
+//---------------------------Dominant Writing Direction--------------------------//
