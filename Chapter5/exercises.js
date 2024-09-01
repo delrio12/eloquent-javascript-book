3751de69690da4dbd6c205590f6c07a0fcfcd60e
Andres Del Rio <addelrio11@gmail.com>
2024-08-31 23:33:43 -0400

    Update exercises.js

--- Chapter5/exercises.js
+++ Chapter5/exercises.js
@@ -1,18 +1,17 @@
 //------------------------Flattening---------------------------//
-//---1---//
 // reduce(), reduce the elements of an array to a single value 
 let arrayOfArrays = [[1, 2, 3], [4, 5], [6]];
 console.log(arrayOfArrays.reduce((accumulator, next) => accumulator.concat(next)));
 
-//---2---//
-
-
-//---3---//
-
 
 //------------------------Your Own Loop--------------------------//
 //---1---//
-
+function loop(value, test, update, execute) {
+    if (test(value)) {
+      execute(value);
+      return loop(update(value), test, update, execute);
+    } // else stop
+  }
 
 //---2---//
 
